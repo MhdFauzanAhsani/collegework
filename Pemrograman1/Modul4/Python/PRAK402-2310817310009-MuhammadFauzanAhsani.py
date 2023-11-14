d436a874400ec64dbea17d50d2f7964b61702950
MhdFauzanAhsani <69187285+MhdFauzanAhsani@users.noreply.github.com>
2023-11-14 09:14:27 +0800

    Update PRAK402-2310817310009-MuhammadFauzanAhsani.py
    kesalahan lagi

--- Pemrograman1/Modul4/Python/PRAK402-2310817310009-MuhammadFauzanAhsani.py
+++ Pemrograman1/Modul4/Python/PRAK402-2310817310009-MuhammadFauzanAhsani.py
@@ -1,9 +1,9 @@
 val = int(input())
 
-for i in range(1, val):
+for i in range(1, val+1):
     if (i % 2 == 1):
         print(f"{i} ", end ="")
 print()
 for j in range(val):
-    if (j % 2 == 0):
+    if ((val - j) % 2 == 0):
         print(f"{val - j} ", end = "")
